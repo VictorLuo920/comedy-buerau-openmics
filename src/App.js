4ef345318d73a5193ae3332dc4826ac0501c17bd
Victor Luo <victorluo920@gmail.com>
2020-05-25 19:23:42 -0700

    added user model and installed react router dom with some test links

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  render() { 
+    return (
+    <>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      Project 4 Start
       </header>
+      <Switch>
+        <Route exact path='/' render={() => {return (<div>Home is Working</div>)}
+        
+        }/>
+        <Route exact path='/signup' render={({ history }) => 
+        { return (<div>Signup is Working</div>)}
+            }/>
+      </Switch>
     </div>
-  );
+    </>
+    );
+  }
 }
 
 export default App;
